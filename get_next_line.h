63c695caa7593f22deb6468e9fd1f446237208f7
nicholas DRIVER <ndriver@c2r6s5.wethinkcode.co.za>
2018-06-27 12:07:18 +0200

    hello

---
+++ get_next_line.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ndriver <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/06/26 10:42:19 by ndriver           #+#    #+#             */
+/*   Updated: 2018/06/26 13:14:06 by ndriver          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include "libft/libft.h"
+# define BUFF_SIZE 42
+
+int		get_next_line(const int fd, char **line);
+
+#endif
